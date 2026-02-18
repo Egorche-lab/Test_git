e20e4e3bc61b6f5eb4b63437bc6e31deb7d46743
Egor <Egorche93@gmail.com>
2026-02-18 18:41:55 +0300

    Домашка управление потоком

--- main.py
+++ main.py
@@ -1,6 +1,57 @@
-print("Hello World")
+# 1
+# total_sum = 0
+# while True:
+#      try:
+#          number = float(input("Введите число: "))
+#          total_sum += number
+#      except:
+#          break
+# print("Сумма чисел:", total_sum)
 
-x = 123
-y = 12
 
-print (x + y)
+
+
+# 2
+# string = input("Введите строку")
+# new_string = ""
+# for i in string:
+#     if i.isupper():
+#      new_string += i
+# print(new_string)
+
+
+
+
+# 3
+# for i in range(1, 101):
+#     if i % 15 == 0:
+#         print("FizzBuzz")
+#     elif i % 3 == 0:
+#         print("Fizz")
+#     elif i % 5 == 0:
+#         print("Buzz")
+#     else:
+#         print(i)
+
+
+
+
+# 4
+# text = input("Введите данные")
+# total_sum = 0
+# for i in text:
+#     if i.isdigit():
+#         total_sum += int(i)
+# print(total_sum)
+
+
+
+
+# 5
+# max_num = float('-inf')
+# for i in range(10):
+#     num = float(input("Введите число"))
+#     if num > max_num:
+#         max_num = num
+# print("Максимальное число", max_num)
+
